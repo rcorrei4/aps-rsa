f3acb3ea4f374d47008628a1bc6381aed13a308f
rcorrei4 <ricardo.csantos0877@gmail.com>
2022-10-31 16:35:49 -0300

    initial commit

--- rsa.py
+++ rsa.py
@@ -4,13 +4,26 @@ def gcd(a, b):
 	else: 
 		return gcd(b, a%b) 
 
-def xgcd(x, y):
-	if y == 0:   
-		return x, 1, 0
-			 
-	d, a, b = xgcd(y, x % y)  
-	 
-	return d, b, a - (x // y) * b
+def xgcd(e, phi):
+	n1 = phi
+	n2 = phi
+	n3 = e
+	n4 = 1
+	while True:
+		n3_new = (n1//n3)*n3
+		n4_new = (n1//n3)*n4
+
+		if n1-n3_new < 0:
+			n1, n3 = n3, (n1-n3_new)%phi
+		else:
+			n1, n3 = n3, n1-n3_new
+		if n2-n4_new < 0:
+			n2, n4 = n4, (n2-n4_new)%phi
+		else:
+			n2, n4  = n4, n2-n4_new
+		if n3 == 1:
+			break
+	return n4
 
 # Números primos
 p = 191
@@ -26,20 +39,18 @@ for e in range(2, phi):
 	if gcd(e, phi) == 1:
 		break
 
-_, d, _ = xgcd(e, phi)
-if d < 0:
-	d= d + phi
+d= xgcd(e, phi)
 
 def encriptar(m):
-	# m^e mod n
 	return pow(m, e) % n
 
 def decriptar(c):
-	# c^d mod n
+	# c^d mod n = m
 	return pow(c, d) % n
 	
-# Mensagem para ser codificada
-m = 321321
+# Mensagem para ser codificada 
+# (Não pode ser maior que N mas isso não vai acontecer porque ao transformar cada letra na table ASCII não vai passar de 200)
+m = 123
 
 print('Chave pública: ', n, e)
 message_in = encriptar(m)
